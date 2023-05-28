2876d5f23eec3d157157806c3f662273afcbeab8
Patrick Sy <patr.sy@gmail.com>
2023-05-28 13:38:17 +0200

    Update DeviceBattery.swift

--- assets/code/DeviceBattery.swift
+++ assets/code/DeviceBattery.swift
@@ -9,29 +9,26 @@
 
 if #available(macOS 13.0, *) {
 
-	let airpods: String = CommandLine.arguments[1]
+	let profiler: String = CommandLine.arguments[1]
 	let devices: String = CommandLine.arguments[2]
 	
-	func getAirpodsInfo(profile: String) -> String? {
-		guard let lowerbound: String.Index = profile.firstRange(of: "Connected:")?.upperBound,
-			  let upperbound: String.Index = profile.firstRange(of: "Not Connected:")?.lowerBound else {
+	func getDeviceInfo(profile: String) -> String? {
+		guard let connected: Substring = profile.connectedDevices else {
 			return nil
 		}
-		let connected: Substring = airpods[lowerbound..<upperbound]
-		if let product = connected.firstMatch(of: #/Air\w+\s?\w+/#),
-		   let caseBatteryLevel = connected.firstMatch(of: #/(?:Case Battery Level:\s)(\d?\d?\d\%)/#),
-		   let leftBatteryLevel = connected.firstMatch(of: #/(?:Left Battery Level:\s)(\d?\d?\d\%)/#),
-		   let rightBatteryLevel = connected.firstMatch(of: #/(?:Right Battery Level:\s)(\d?\d?\d\%)/#)
-		{
-			let productName: Substring = connected[product.range]
-			let caseBatteryPercent: Substring = caseBatteryLevel.output.1
-			let leftBatteryPercent: Substring = leftBatteryLevel.output.1
-			let rightBatteryPercent: Substring = rightBatteryLevel.output.1
-			// TODO: Identify versions
-			let item: String = "{\"title\":\"\(productName)\",\"subtitle\":\"􀹬 \(caseBatteryPercent) 􀲎 \(leftBatteryPercent) 􀲍 \(rightBatteryPercent)\",\"valid\":\"false\",\"icon\":{\"path\":\"icons/airpodspro.png\"}}"
-			return item
+		var items: [String] = []
+		let devices: [Substring] = connected.split(separator: #/>\n/#)
+		for device in devices where device.has(substring: "Battery Level") {
+			if let airpodsItem: String = device.airpodsItem {
+				items.append(airpodsItem)
+				continue
+			}
+			if let deviceItem: String = device.deviceItem {
+				items.append(deviceItem)
+			}
 		}
-		return nil
+		guard !items.isEmpty else { return nil }
+		return items.joined(separator: ",")
 	}
 	
 	func getBluetoothDeviceInfo(ioreg: String) -> String? {
@@ -51,11 +48,11 @@ if #available(macOS 13.0, *) {
 		return items.joined(separator: ",")
 	}
 	
-	let infoAirpods: String? = getAirpodsInfo(profile: airpods)
+	let infoProfiler: String? = getDeviceInfo(profile: profiler)
 	let infoDevices: String? = getBluetoothDeviceInfo(ioreg: devices)
 	
 	var response: String = "{\"items\":["
-	switch (infoAirpods, infoDevices) {
+	switch (infoProfiler, infoDevices) {
 	case (.some(let a), .some(let d)): response += "\(a),\(d)"
 	case (.some(let a), .none): response += a
 	case (.none, .some(let d)): response += d
@@ -70,6 +67,42 @@ if #available(macOS 13.0, *) {
 
 
 extension Substring {
+	
+	func has(substring: String) -> Bool {
+		firstRange(of: substring) != nil
+	}
+	
+	var airpodsItem: String? {
+		if let product = firstMatch(of: #/Air\w+\s?\w+/#),
+		   let caseBatteryLevel = firstMatch(of: #/(?:Case Battery Level:\s)(\d?\d?\d\%)/#),
+		   let leftBatteryLevel = firstMatch(of: #/(?:Left Battery Level:\s)(\d?\d?\d\%)/#),
+		   let rightBatteryLevel = firstMatch(of: #/(?:Right Battery Level:\s)(\d?\d?\d\%)/#)
+		{
+			let productName: Substring = self[product.range]
+			let caseBatteryPercent: Substring = caseBatteryLevel.output.1
+			let leftBatteryPercent: Substring = leftBatteryLevel.output.1
+			let rightBatteryPercent: Substring = rightBatteryLevel.output.1
+			// TODO: Identify versions
+			let item: String = "{\"title\":\"\(productName)\",\"subtitle\":\"􀹬 \(caseBatteryPercent) 􀲎 \(leftBatteryPercent) 􀲍 \(rightBatteryPercent)\",\"valid\":\"false\",\"icon\":{\"path\":\"icons/airpodspro.png\"}}"
+			return item
+		}
+		return nil
+	}
+	
+	var deviceItem: String? {
+		if let product = firstMatch(of: #/^\s+(.*):\n/#),
+		   let battery = firstMatch(of: #/(?:Battery Level:\s)(\d?\d?\d)/#)
+		{
+			let productName: Substring = product.output.1
+			let batterPercent: Substring = battery.output.1
+			let battery: String = Int(batterPercent)!.battery
+			//let icon: String = productName.icon
+			let icon: String = self.icon
+			return "{\"title\":\"\(productName)\",\"subtitle\":\"\(batterPercent)% \(battery)\",\"valid\":\"false\",\"icon\":{\"path\":\"\(icon)\"}}"
+		}
+		return nil
+	}
+	
 	// TODO: Determine `Minor Type` and hash with `DeviceAddress` (profiler input)
 	var icon: String {
 		switch true {
@@ -77,6 +110,7 @@ extension Substring {
 		case firstRange(of: #/[Tt]rack/#) != nil: return "icons/trackpad.png"
 		case firstRange(of: #/Magic Mo/#) != nil: return "icons/mmouse.png"
 		case firstRange(of: #/[Mm]ouse/#) != nil: return "icons/mouse.png"
+		case firstRange(of: #/Head/#) != nil: return "icons/headphones.png" // TODO: headset.png
 		default: return ""
 		}
 	}
@@ -94,3 +128,12 @@ extension Int {
 	}
 }
 
+extension String {
+	var connectedDevices: Substring? {
+		guard let lowerbound: String.Index = firstRange(of: "Connected:")?.upperBound,
+			  let upperbound: String.Index = firstRange(of: "Not Connected:")?.lowerBound else {
+			return nil
+		}
+		return self[lowerbound..<upperbound]
+	}
+}
