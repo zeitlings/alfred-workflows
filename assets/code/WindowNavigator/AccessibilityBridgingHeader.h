e47ed277fd4d684ddcdfeebc9f8f42aaf3f92eb2
Patrick Sy <patr.sy@gmail.com>
2024-06-16 16:29:25 +0200

    Update Assets

--- assets/code/WindowNavigator/AccessibilityBridgingHeader.h
+++ assets/code/WindowNavigator/AccessibilityBridgingHeader.h
@@ -7,9 +7,10 @@
 
 #ifndef AccessibilityBridgingHeader_h
 #define AccessibilityBridgingHeader_h
-#endif /* AccessibilityBridgingHeader_h */
 
 #import <AppKit/AppKit.h>
 
 AXError _AXUIElementGetWindow(AXUIElementRef element, uint32_t *identifier);
+
+#endif /* AccessibilityBridgingHeader_h */
 
