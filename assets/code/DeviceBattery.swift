07118a58ee7fe49421d6c0de19ea7b6a93a5c2e4
Patrick Sy <patr.sy@gmail.com>
2025-02-21 09:49:55 +0100

    Device Battery v2.1.0

--- assets/code/DeviceBattery.swift
+++ assets/code/DeviceBattery.swift
@@ -6,6 +6,7 @@
 //
 //  Created by Patrick Sy on 29/04/2023.
 //  Updated 11/02/2025 (macOS 15)
+//  Updated 21/02/2025
 //
 
 if #available(macOS 13.0, *) {
@@ -13,15 +14,19 @@ if #available(macOS 13.0, *) {
 	let profiler: String = CommandLine.arguments[1] // `system_profiler`
 	let devices: String = CommandLine.arguments[2]  // `ioreg`
 
-	struct Device {
+    struct Device {
         let name: String
         let address: String
         var batteryLevel: Int?
+        var minorType: String?
         var isAirPods: Bool = false
         var airPodsLevels: (left: String, right: String, case: String)?
 
         var icon: String {
-            name[name.startIndex...].icon
+            if let minorType: String {
+                return minorType[minorType.startIndex...].icon
+            }
+            return name[name.startIndex...].icon
         }
 
         var alfredItem: String {
@@ -35,31 +40,36 @@ if #available(macOS 13.0, *) {
     func parseSystemProfiler(_ input: String) -> [String: Device] {
         var devices: [String: Device] = [:]
         guard let connected = input.connectedDevices else { return devices }
-
         let deviceEntries = connected.split(separator: #/>\n/#)
         for entry in deviceEntries {
             if entry.contains("AirPods") {
                 if let nameMatch = entry.firstMatch(of: #/^\s+(.+):\n/#),
                    let addressMatch = entry.firstMatch(of: #/Address:\s+([0-9A-F:]+)/#),
                    let leftBattery = entry.firstMatch(of: #/Left Battery Level:\s+(\d+%)/#),
-                   let rightBattery = entry.firstMatch(of: #/Right Battery Level:\s+(\d+%)/#) {
-
+                   let rightBattery = entry.firstMatch(of: #/Right Battery Level:\s+(\d+%)/#)
+                {
                     let name = String(nameMatch.output.1)
                     let address = String(addressMatch.output.1).lowercased()
+                    let caseBattery: String = entry.firstMatch(of: #/(?:Case Battery Level:\s)(\d?\d?\d\%)/#).map({ String($0.output.1) }) ?? "n/A"
                     var device = Device(name: name, address: address)
                     device.isAirPods = true
                     device.airPodsLevels = (
                         left: String(leftBattery.output.1),
                         right: String(rightBattery.output.1),
-                        case: "n/A" // Case battery level might not be available anymore
+                        case: caseBattery // Case battery level might not be available anymore
                     )
                     devices[address] = device
                 }
             } else if let nameMatch = entry.firstMatch(of: #/^\s+(.+):\n/#),
-                      let addressMatch = entry.firstMatch(of: #/Address:\s+([0-9A-F:]+)/#) {
-                let name = String(nameMatch.output.1)
-                let address = String(addressMatch.output.1).lowercased()
-                devices[address] = Device(name: name, address: address)
+                      let addressMatch = entry.firstMatch(of: #/Address:\s+([0-9A-F:]+)/#)
+            {
+                let batteryMatch = entry.firstMatch(of: #/Battery Level:\s(\d?\d?\d)/#)
+                let typeMatch = entry.firstMatch(of: #/Type:\s+(.+)/#)
+                let name: String = String(nameMatch.output.1)
+                let address: String = String(addressMatch.output.1).lowercased()
+                let battery: Optional<Int> = batteryMatch.map({ Int($0.output.1) }) ?? nil
+                let type: Optional<String> = typeMatch.map({ String($0.output.1) }) ?? nil
+                devices[address] = Device(name: name, address: address, batteryLevel: battery, minorType: type)
             }
         }
         return devices
@@ -68,7 +78,6 @@ if #available(macOS 13.0, *) {
     func parseIOReg(_ input: String) -> [String: Int] {
         var batteryLevels: [String: Int] = [:]
         let devices = input.split(separator: #/\+-o/#)
-
         for device in devices {
             if let addressMatch = device.firstMatch(of: #/"DeviceAddress"\s+=\s+"([0-9a-f-]+)"/#),
                let batteryMatch = device.firstMatch(of: #/"BatteryPercent"\s+=\s+(\d+)/#) {
@@ -104,10 +113,10 @@ if #available(macOS 13.0, *) {
             .map { $0.alfredItem }
 
         guard !items.isEmpty else {
-            return "{\"items\":[{\"title\":\"No Bluetooth devices connected.\"}],\"rerun\":4}"
+            return "{\"items\":[{\"title\":\"No Bluetooth devices connected.\"}]}"
         }
 
-        return "{\"items\":[\(items.joined(separator: ","))],\"rerun\":4}"
+        return "{\"items\":[\(items.joined(separator: ","))],\"rerun\":2}"
     }
 
 	let response = processDeviceInfo(profiler: profiler, ioreg: devices)
@@ -117,44 +126,12 @@ if #available(macOS 13.0, *) {
 	print("{\"items\":[{\"title\":\"The workflow requires macOS 13.0 or later\"}]}", terminator: "")
 }
 
-
 extension Substring {
 
 	func has(substring: String) -> Bool {
 		firstRange(of: substring) != nil
 	}
 
-	var airpodsItem: String? {
-		if let product = firstMatch(of: #/Air\w+\s?\w+/#),
-		   let caseBatteryLevel = firstMatch(of: #/(?:Case Battery Level:\s)(\d?\d?\d\%)/#),
-		   let leftBatteryLevel = firstMatch(of: #/(?:Left Battery Level:\s)(\d?\d?\d\%)/#),
-		   let rightBatteryLevel = firstMatch(of: #/(?:Right Battery Level:\s)(\d?\d?\d\%)/#)
-		{
-			let productName: Substring = self[product.range]
-			let caseBatteryPercent: Substring = caseBatteryLevel.output.1
-			let leftBatteryPercent: Substring = leftBatteryLevel.output.1
-			let rightBatteryPercent: Substring = rightBatteryLevel.output.1
-			// TODO: Identify versions
-			let item: String = "{\"title\":\"\(productName)\",\"subtitle\":\"􀹬 \(caseBatteryPercent) 􀲎 \(leftBatteryPercent) 􀲍 \(rightBatteryPercent)\",\"valid\":\"false\",\"icon\":{\"path\":\"icons/airpodspro.png\"}}"
-			return item
-		}
-		return nil
-	}
-
-	var deviceItem: String? {
-		if let product = firstMatch(of: #/^\s+(.*):\n/#),
-		   let battery = firstMatch(of: #/(?:Battery Level:\s)(\d?\d?\d)/#)
-		{
-			let productName: Substring = product.output.1
-			let batterPercent: Substring = battery.output.1
-			let battery: String = Int(batterPercent)!.battery
-			let icon: String = self.icon
-			return "{\"title\":\"\(productName)\",\"subtitle\":\"\(batterPercent)% \(battery)\",\"valid\":\"false\",\"icon\":{\"path\":\"\(icon)\"}}"
-		}
-		return nil
-	}
-
-	// TODO: Determine `Minor Type` and hash with `DeviceAddress` (profiler input)
 	var icon: String {
 		switch true {
 		case firstRange(of: #/[Kk]ey/#)   != nil: return "icons/keyboard.png"
